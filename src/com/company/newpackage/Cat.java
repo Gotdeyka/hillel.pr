6419cdeb2de2a6006ffe6addd8298c7e0bbe110f
Gordey <g.timofeev1@gmail.com>
2019-11-23 12:59:12 +0200

    test

---
+++ src/com/company/newpackage/Cat.java
@@ -0,0 +1,40 @@
+package com.company.newpackage;
+
+public class Cat {
+
+    public Cat() {
+    }
+
+
+  //  String name;
+    //int age = 10;
+    //String wth = "dntknw";
+    //int weight = 3;
+  //String color = "blue";
+
+    //public Cat(String name, int age, String wth, int weight, String color) {
+      //  this.name = name;
+        //this.age = age;
+        //this.wth = wth;
+        //this.weight = weight;
+        //this.color = color;
+
+
+    String name;
+    int weight;
+    int age;
+    String color;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+
+    public Cat(String name, int weight, int age) {
+        this.name = name;
+    }
+
+    public Cat(String name, int weight, int age, String color) {
+        this.name = name;
+    }
+}
